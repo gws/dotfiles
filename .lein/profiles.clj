2dcc79183354cbb3852369b61952d90dbe370c78
Gordon Stratton <gordon.stratton@gmail.com>
2016-08-20 20:05:25 +0000

    Add Clojure exclusions for dependencies that specify one explicitly

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,10 +2,10 @@
         :dependencies [[org.clojure/tools.namespace "0.2.10"]
                        [org.clojure/tools.trace "0.7.9"]
                        [slamhound "1.5.5"]]
-        :plugins [[jonase/eastwood "0.2.3"]
+        :plugins [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
                   [lein-ancient "0.6.10"]
-                  [lein-cljfmt "0.5.3"]
-                  [lein-kibit "0.1.2"]
+                  [lein-cljfmt "0.5.3" :exclusions [org.clojure/clojure]]
+                  [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]
                   [lein-ring "0.9.7"]
                   [lein-pprint "1.1.2"]]
         :signing {:gpg-key "0x5D5F3485F7935DE0"}}}
