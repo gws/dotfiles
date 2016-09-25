3cc799095d47f612715bed1e408a2c0c38531510
Gordon Stratton <gordon.stratton@gmail.com>
2016-09-25 19:53:24 +0000

    Bump Leiningen user profile dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,7 +2,7 @@
                        [org.clojure/tools.trace "0.7.9"]]
         :plugins [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
                   [lein-ancient "0.6.10"]
-                  [lein-cljfmt "0.5.3" :exclusions [org.clojure/clojure]]
+                  [lein-cljfmt "0.5.5" :exclusions [org.clojure/clojure]]
                   [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]
                   [lein-ring "0.9.7"]
                   [lein-pprint "1.1.2"]]
