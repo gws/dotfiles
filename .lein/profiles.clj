171b5149b730f3e942fa7487d211642d1f7e44b2
Gordon Stratton <gordon.stratton@gmail.com>
2016-12-05 01:45:57 +0000

    Bump lein plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,9 +2,9 @@
                        [org.clojure/tools.trace "0.7.9"]]
         :plugins [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
                   [lein-ancient "0.6.10"]
-                  [lein-cljfmt "0.5.5" :exclusions [org.clojure/clojure]]
+                  [lein-cljfmt "0.5.6" :exclusions [org.clojure/clojure]]
                   [lein-exec "0.3.6"]
-                  [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]
-                  [lein-ring "0.9.7"]
+                  [lein-kibit "0.1.3" :exclusions [org.clojure/clojure]]
+                  [lein-ring "0.10.0"]
                   [lein-pprint "1.1.2"]]
         :signing {:gpg-key "0x5D5F3485F7935DE0"}}}
