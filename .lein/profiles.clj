366425b5b64d38e8c4cea4344f0b4fe639ee1fba
Gordon Stratton <gordon.stratton@gmail.com>
2018-08-09 23:04:44 +0000

    Update cider-nrepl to 0.18.0

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                        [org.clojure/tools.trace "0.7.9"]]
-        :plugins [[cider/cider-nrepl "0.17.0"]
+        :plugins [[cider/cider-nrepl "0.18.0"]
                   [jonase/eastwood "0.2.9"]
                   [lein-ancient "0.6.15"]
                   [lein-cljfmt "0.6.0"]
