2a1cf53532af62772829b90ceb1ab721dd05f242
Gordon Stratton <gordon.stratton@gmail.com>
2018-12-15 02:19:25 +0000

    Update lein-cljfmt to 0.6.3

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,7 +3,7 @@
         :plugins [[cider/cider-nrepl "0.18.0"]
                   [jonase/eastwood "0.3.4"]
                   [lein-ancient "0.6.15"]
-                  [lein-cljfmt "0.6.2"]
+                  [lein-cljfmt "0.6.3"]
                   [lein-exec "0.3.7"]
                   [lein-kibit "0.1.6"]
                   [lein-ring "0.12.4"]
