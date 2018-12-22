c8bb9f29c98a889472bdc956413b3b7794e5ecef
Gordon Stratton <gordon.stratton@gmail.com>
2018-12-22 20:33:00 +0000

    Move cider-nrepl into the `:repl` profile
    This speeds up Leiningen boot time in the non-`lein repl` case.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,10 @@
 {:user {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                        [org.clojure/tools.trace "0.7.10"]]
-        :plugins [[cider/cider-nrepl "0.18.0"]
-                  [jonase/eastwood "0.3.4"]
+        :plugins [[jonase/eastwood "0.3.4"]
                   [lein-ancient "0.6.15"]
                   [lein-cljfmt "0.6.3"]
                   [lein-exec "0.3.7"]
                   [lein-kibit "0.1.6"]
                   [lein-ring "0.12.4"]
-                  [lein-pprint "1.2.0"]]}}
+                  [lein-pprint "1.2.0"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.18.0"]]}}
