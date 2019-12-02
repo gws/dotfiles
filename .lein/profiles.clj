52ac09a4922b78289bb89716018a9279836f3ee1
Gordon Stratton <gordon.stratton@gmail.com>
2019-12-02 09:26:34 -0800

    Update default dependencies used with Leiningen

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,9 +2,9 @@
                        [org.clojure/tools.trace "0.7.10"]]
         :plugins [[jonase/eastwood "0.3.6"]
                   [lein-ancient "0.6.15"]
-                  [lein-cljfmt "0.6.4"]
+                  [lein-cljfmt "0.6.6"]
                   [lein-exec "0.3.7"]
-                  [lein-kibit "0.1.7"]
+                  [lein-kibit "0.1.8"]
                   [lein-ring "0.12.5"]
                   [lein-pprint "1.2.0"]]}
  :repl {:plugins [[cider/cider-nrepl "0.22.4"]]}}
