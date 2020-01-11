a28d28efafa783115a7afd88604ca64a13dabc0c
Gordon Stratton <gordon.stratton@gmail.com>
2020-01-10 19:34:03 -0800

    Update clj-kondo

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:dependencies [[org.clojure/tools.namespace "0.3.1"]
                        [org.clojure/tools.trace "0.7.10"]
-                       [clj-kondo "2019.12.14"]]
+                       [clj-kondo "2020.01.10"]]
         :plugins [[lein-ancient "0.6.15"]
                   [lein-cljfmt "0.6.6"]
                   [lein-exec "0.3.7"]
