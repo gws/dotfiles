b54af725f3b5b49444e6a65863cee1e7d670d8b2
Gordon Stratton <gordon.stratton@gmail.com>
2020-02-17 07:26:25 -0800

    Update lein user profile packages

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,11 @@
 {:user {:dependencies [[org.clojure/tools.namespace "0.3.1"]
                        [org.clojure/tools.trace "0.7.10"]
-                       [clj-kondo "2020.01.10"]]
+                       [clj-kondo "2020.01.27"]]
         :plugins [[lein-ancient "0.6.15"]
                   [lein-cljfmt "0.6.6"]
                   [lein-exec "0.3.7"]
                   [lein-kibit "0.1.8"]
                   [lein-ring "0.12.5"]
-                  [lein-pprint "1.2.0"]]
+                  [lein-pprint "1.3.2"]]
         :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]}}
- :repl {:plugins [[cider/cider-nrepl "0.22.4"]]}}
+ :repl {:plugins [[cider/cider-nrepl "0.24.0"]]}}
