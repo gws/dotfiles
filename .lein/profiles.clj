bc5080be33287afd7e7681d4d300efbc880388f4
Gordon Stratton <gordon.stratton@gmail.com>
2021-12-28 08:19:53 -0800

    Update Leiningen dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:dependencies [[org.clojure/tools.namespace "1.2.0"]
                        [org.clojure/tools.trace "0.7.11"]
-                       [clj-kondo "2021.12.01"]]
+                       [clj-kondo "2021.12.19"]]
         :plugins [[lein-ancient "0.7.0"]
                   [lein-cljfmt "0.8.0"]
                   [lein-kibit "0.1.8"]
