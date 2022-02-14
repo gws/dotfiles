74de44b6bd3630e709d55b872d2b203d7f2be912
Gordon Stratton <gordon.stratton@gmail.com>
2022-02-13 21:53:24 -0800

    Update Clojure dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:dependencies [[org.clojure/tools.namespace "1.2.0"]
                        [org.clojure/tools.trace "0.7.11"]
-                       [clj-kondo "2022.01.15"]]
+                       [clj-kondo "2022.02.09"]]
         :plugins [[lein-ancient "0.7.0"]
                   [lein-cljfmt "0.8.0"]
                   [lein-kibit "0.1.8"]
